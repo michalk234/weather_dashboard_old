21acde616307f0eed3d001d4475cc22245aa8dc6
michalk234 <100431431+michalk234@users.noreply.github.com>
2026-04-10 23:35:44 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { weatherRoutes } from "./routes/weatherRoutes.js";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
